cd65983dd1ecee2ebb46882bd95ffb1d621259e1
gsoftdev95 <gesoftdev@gmail.com>
2025-04-02 15:31:16 -0500

    Actualizaciones de UI

--- QuienesSomos.php
+++ QuienesSomos.php
@@ -1,34 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>RATS PROTECCION PERU</title>
-        <meta name="Author" lang="es" content="Gean Huaman">
-        <meta name="keywords" content="Protección, Protecciones, Seguridad, Rats, Rats protectio">
-        <meta name="description" content="RATS PROTECTION es una marca peruana de equipos de protección especializada en deportes extremos como MTB, BMX, skate y roller. Ofrecemos productos innovadores y seguros para hombres, mujeres y niños." />
-        <meta name="copyright" content="&copy; 2024 RATS PROTECTION. Todos los derechos reservados." />
-        <meta name="robots" content="index, follow">
-        <link rel="icon" href="./iconos/externos/logoUnificado.ico">
-        <link rel="stylesheet" href="./css/estilosRatsgrid.css">
-        <link rel="stylesheet" href="./iconos/fonts/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <?php include_once('./partials/head.php')?>
     </head>
 <body>
     <header>
-        <nav id="encabezado">
-            <div class="enc1">Centro de ayuda</div>
-            <div class="enc1"><a href="Registro">Registrate</a></div>
-            <div class="enc1"><a href="IniciarSesion">Iniciar Sesión</a></div>
-        </nav>
-        <section id="logymen">
-            <a href="index"><div class="logorats"></div></a>            
-        </section>
+        <?php include_once('./partials/navBar.php')?>
     </header>
     
     <aside class="ratswsp">
